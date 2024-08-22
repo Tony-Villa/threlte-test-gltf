0df985df8e496ed499022882bf32891fe3b02dbe
Tony-Villa <tonyvilla77@gmail.com>
2024-08-21 17:55:20 -0700

    Initial commit

---
+++ src/lib/components/Scene.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { T } from '@threlte/core'
+  import { Grid, OrbitControls } from '@threlte/extras'
+	import Spaceship from './models/spaceship.svelte';
+</script>
+
+<T.PerspectiveCamera makeDefault position={[-5, 6, 10]} fov={25}>
+	<OrbitControls enableDamping target={[0, 0, 0]} />
+</T.PerspectiveCamera>
+
+<T.DirectionalLight intensity={1.8} position={[0, 10, 0]} castShadow shadow.bias={-0.0001} />
+<T.AmbientLight intensity={0.2} />
+
+<Grid
+	position.y={-0.001}
+	cellColor="#ffffff"
+	sectionColor="#ffffff"
+	sectionThickness={0}
+	fadeDistance={50}
+	cellSize={2}
+  axis
+/>
+
+<Spaceship />
+
---
+++ src/lib/types/types.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export interface PropTypes {
+  fallback?: any
+  error?: any
+  children?: any
+  ref?: any
+  props?: any
+}
